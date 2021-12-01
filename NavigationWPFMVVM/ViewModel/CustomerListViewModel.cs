f971ed89a0f7314d3c4f749db171c6a1d51012d5
Yuri <gagi.shmagi@gmail.com>
2021-12-01 03:19:56 -0800

    Added BindableBase for some reason didn't show

--- NavigationWPFMVVM/ViewModel/CustomerListViewModel.cs
+++ NavigationWPFMVVM/ViewModel/CustomerListViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NavigationWPFMVVM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace NavigationWPFMVVM.ViewModel
 {
-    internal class CustomerListViewModel
+    internal class CustomerListViewModel :BindableBase
     {
     }
 }
