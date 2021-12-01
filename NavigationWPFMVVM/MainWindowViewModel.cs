2954dcf1c8bc2e52cf651d284bdd9262b0447309
Yuri <gagi.shmagi@gmail.com>
2021-12-01 02:11:04 -0800

    Add project files.

---
+++ NavigationWPFMVVM/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using NavigationWPFMVVM.Models;
+using NavigationWPFMVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavigationWPFMVVM
+{
+    internal class MainWindowViewModel : BindableBase
+    {
+
+        public MainWindowViewModel()
+        {
+            NavCommand = new MyICommand<string>(OnNav);
+        }
+
+        private CustomerListViewModel custListViewModel = new CustomerListViewModel();
+
+        private OrderListViewModel orderViewModelModel = new OrderListViewModel();
+
+        private BindableBase _currentViewModel;
+
+        public BindableBase CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set { SetProperty(ref _currentViewModel, value); }
+        }
+
+        public MyICommand<string> NavCommand { get; private set; }
+
+        private void OnNav(string destination)
+        {
+
+            switch (destination)
+            {
+                case "orders":
+                    CurrentViewModel = orderViewModelModel;
+                    break;
+                case "customers":
+                default:
+                    CurrentViewModel = custListViewModel;
+                    break;
+            }
+        }
+    }
+}
